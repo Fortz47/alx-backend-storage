aec97acac9a451988b7555e8c53d577264a5a89d
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-21 16:54:38 +0100

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER reset_email AFTER UPDATE
 ON `users` FOR EACH ROW
 BEGIN
 IF NEW.email != OLD.email THEN
-  UPDATE `users` SET valid_email = 0
+  UPDATE `users` SET valid_email = IF(OLD.valid_email = 1, 0, 1)
   WHERE email = NEW.email;
 END $$
 DELIMITER ;
