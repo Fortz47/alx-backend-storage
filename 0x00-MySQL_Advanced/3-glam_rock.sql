a148c8da963db1ac657ec17769542a5826733ccc
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-21 15:04:07 +0100

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style
 
-SELECT `band_name`, IF(`formed` < '2024', '2024' - `formed`, '0') AS lifespan
+SELECT `band_name`, IF(`formed` < '2022', '2022' - `formed`, '0') AS lifespan
 FROM `metal_bands`
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
