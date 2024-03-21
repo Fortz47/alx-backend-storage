b769fd051927ec94873b5e9a0ffd8e21388f6a8e
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-22 00:43:22 +0100

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,12 +6,10 @@ DELIMITER $$
 CREATE TRIGGER reset_email BEFORE UPDATE
 ON `users` FOR EACH ROW
 BEGIN
-  DECLARE v_email BOOLEAN DEFAULT 0;
-  IF NEW.email != OLD.email THEN
-    IF NEW.valid_email = 0 THEN
-      SET v_email = 1;
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
     END IF;
-    SET NEW.valid_email = v_email;
-  END IF;
 END $$
 DELIMITER ;
