bb37905cf82aaf5cfe629730b306111387aa28d6
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-21 17:12:08 +0100

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -7,7 +7,8 @@ ON `users` FOR EACH ROW
 BEGIN
   IF NEW.email != OLD.email THEN
     SET NEW.valid_email = 0;
-  ELSE SET NEW.valid_email = NEW.valid_email;
+  ELSE
+    SET NEW.valid_email = NEW.valid_email;
   END IF;
 END $$
 DELIMITER ;
