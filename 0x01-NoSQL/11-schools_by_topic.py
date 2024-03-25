202d841e6e0ea169703e2373f5909c003d8b0051
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-25 22:23:34 +0100

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""returns list of school having specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns list of school having specific topic"""
+    schools = mongo_collection.find()
+    res = [sch for sch in schools if topic in sch.topic]
+    return res
