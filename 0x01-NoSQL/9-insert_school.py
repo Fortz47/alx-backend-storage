0b2f713488f25455fb83787d512368c917b53aed
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-25 21:57:06 +0100

    task 8

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    newDoc = Dict(**kwargs)
+    res = mongo_collection.insert_one(newDoc)
+    return res.inserted_id
