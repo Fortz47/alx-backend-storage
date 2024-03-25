d8be211ece5fd7cb31e97774ac403f3302370c81
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-25 21:47:14 +0100

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""lists all documents in a collection:"""
+
+
+if __name__ == "__main__":
+    def list_all(mongo_collection):
+        """lists all documents in a collection"""
+        all_docs = []
+        for doc in mongo_collection.find():
+            all_docs.append(doc)
+        return all_docs
