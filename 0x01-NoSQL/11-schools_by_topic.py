e69ada14a7cc9e316f2f07513b697d8d2f131fa3
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-25 22:47:27 +0100

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,10 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns list of school having specific topic"""
-    schools = mongo_collection.find()
-    res = []
-    for sch in schools:
-        if hasattr(sch, 'topic') and isinstance(sch.topic, str):
-            if topic in sch.topic:
-                res.append(sch)
-    return res
+    schools = mongo_collection.find({"topics": {"$in": [topic]}})
+    return list(schools)
