74ae0b7a965fda5b989b52b79bede1baf653c6cb
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-26 22:20:50 +0100

    task 101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    students = mongo_collection.find()
+    for student in students:
+        val = sum(1 for i in student.topics if i.score and i.score >= 0)
+        averageScore = sum(i.score for i in student.topics if i.score and i.score >= 0) / val
+        mongo_collection.update({'_id': student._id}, {$push : {'topics': {'averageScore': averageScore}}})
+    return mongo_collection.find()
