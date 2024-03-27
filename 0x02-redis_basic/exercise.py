93c8655f92c2b906bf52e236cf6d22684fe2647f
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-27 18:15:30 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """creats a cache class thats connects to a redis db"""
 import redis
 from uuid import uuid4
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -11,23 +12,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """"store item in db"""
-        id = str(uuid4())
-        self._redis.mset({id: data})
-        return id
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
 
-     def get(self, key, fn=None):
+     def get(self, key: str, fn: Optional[Callable] = None) -> str:
         """Retrieve data from db"""
         data = self._redis.get(key)
         if data is None:
             return None
         return fn(data) if fn else data
 
-    def get_str(self, key):
+    def get_str(self, key: str) -> str:
         """Retrieve string data from db"""
-        return self.get(key, fn=lambda x: x.decode("utf-8"))
+        fn = lambda x: x.decode("utf-8")
+        return self.get(key, fn)
 
-    def get_int(self, key):
+    def get_int(self, key: str) -> str:
         """Retrieve integer data from db"""
-        return self.get(key, fn=int)
+        return self.get(key, int)
