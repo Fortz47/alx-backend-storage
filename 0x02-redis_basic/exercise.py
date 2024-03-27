9f299bde9320278b58b4396a6c8f9a0fbe24bd4c
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-27 18:06:06 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,18 @@ class Cache:
         id = str(uuid4())
         self._redis.mset({id: data})
         return id
+
+     def get(self, key, fn=None):
+        """Retrieve data from db"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key):
+        """Retrieve string data from db"""
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key):
+        """Retrieve integer data from db"""
+        return self.get(key, fn=int)
