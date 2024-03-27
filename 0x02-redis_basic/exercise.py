e957b832d358879db5cedf969210d956ed42e833
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-27 17:35:27 +0100

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,8 +7,8 @@ from uuid import uuid4
 class Cache:
     """creats a cache class thats connects to a redis db"""
     def __init__(self):
-        _redis = redis.Redis()
-        _redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data):
         id = str(uuid4())
