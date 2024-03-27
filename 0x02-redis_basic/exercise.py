5b2a03a9183044156a6fb8bb8042fb95ff11e895
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-27 17:23:15 +0100

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""creats a cache class thats connects to a redis db"""
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """creats a cache class thats connects to a redis db"""
+    def __init__():
+        _redis = redis.Redis()
+        _redis.flushdb()
+
+    def store(data):
+        id = uuid4()
+        self._redis.set(id, data)
+        return id
